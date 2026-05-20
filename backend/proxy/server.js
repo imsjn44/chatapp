02bfc450698bf0316c8a484889cc805520bffbb8
Sujan  Timalsina <iamsjn44@gmail.com>
2026-05-20 15:56:57 +0545

    Updated package.json for proxy

--- backend/proxy/server.js
+++ backend/proxy/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();

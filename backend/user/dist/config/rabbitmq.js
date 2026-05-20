81683f6b0c93cd76ddb2e64d3037f162124fcf4a
Sujan  Timalsina <iamsjn44@gmail.com>
2026-05-20 20:19:21 +0545

    port added on ampqlib

--- backend/user/dist/config/rabbitmq.js
+++ backend/user/dist/config/rabbitmq.js
@@ -1,27 +1,27 @@
 import amqplib from "amqplib";
 let channel;
 export const connectRabbitMQ = async () => {
-    try {
-        const connection = await amqplib.connect({
-            protocol: "amqp",
-            hostname: process.env.RABBITMQ_HOST,
-            username: process.env.RABBITMQ_USERNAME,
-            password: process.env.RABBITMQ_PASSWORD,
-        });
-        channel = await connection.createChannel();
-        console.log("✅Rabbit MQ connected");
-    }
-    catch (error) {
-        console.log("Failed to connect to rabbitmq", error);
-    }
+  try {
+    const connection = await amqplib.connect({
+      protocol: "amqp",
+      port: 443,
+      hostname: process.env.RABBITMQ_HOST,
+      username: process.env.RABBITMQ_USERNAME,
+      password: process.env.RABBITMQ_PASSWORD,
+    });
+    channel = await connection.createChannel();
+    console.log("✅Rabbit MQ connected");
+  } catch (error) {
+    console.log("Failed to connect to rabbitmq", error);
+  }
 };
 export const publishToQueue = async (queueName, message) => {
-    if (!channel) {
-        console.log("RabbitMQ channel not initialized");
-        return;
-    }
-    await channel.assertQueue(queueName, { durable: true });
-    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
-        persistent: true,
-    });
+  if (!channel) {
+    console.log("RabbitMQ channel not initialized");
+    return;
+  }
+  await channel.assertQueue(queueName, { durable: true });
+  channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+    persistent: true,
+  });
 };

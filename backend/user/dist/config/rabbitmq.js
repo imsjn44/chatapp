b4f3aa59b3a534e8ba21870cc324aa690b40cab9
Sujan  Timalsina <iamsjn44@gmail.com>
2026-05-21 09:28:33 +0545

    rabbitmq url added

--- backend/user/dist/config/rabbitmq.js
+++ backend/user/dist/config/rabbitmq.js
@@ -2,13 +2,14 @@ import amqplib from "amqplib";
 let channel;
 export const connectRabbitMQ = async () => {
   try {
-    const connection = await amqplib.connect({
-      protocol: "amqp",
-      port: 443,
-      hostname: process.env.RABBITMQ_HOST,
-      username: process.env.RABBITMQ_USERNAME,
-      password: process.env.RABBITMQ_PASSWORD,
-    });
+    // const connection = await amqplib.connect({
+    //   protocol: "amqp",
+    //   hostname: process.env.RABBITMQ_HOST,
+    //   username: process.env.RABBITMQ_USERNAME,
+    //   password: process.env.RABBITMQ_PASSWORD,
+    // });
+    console.log(process.env.RABBITMQ_URL);
+    const connection = await amqplib.connect(process.env.RABBITMQ_URL);
     channel = await connection.createChannel();
     console.log("✅Rabbit MQ connected");
   } catch (error) {
